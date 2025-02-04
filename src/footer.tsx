87156ba535ac3830fbeef333e3878bc78d342171
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2025-02-04 17:46:38 -0500

    Update footer.tsx

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const footer = () => {
+const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <p>© 2025 A.M.O.S. All rights reserved.</p>
@@ -8,4 +8,4 @@ const footer = () => {
   );
 };
 
-export default footer;
+export default Footer;
